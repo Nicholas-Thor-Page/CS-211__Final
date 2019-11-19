7fd54dee5fed653f924a1e1f9d4617a28995a157
cwalsh22 <57801143+cwalsh22@users.noreply.github.com>
2019-11-19 10:50:07 -0500

    Create FileReader.java

---
+++ FileReader.java
@@ -0,0 +1,18 @@
+import java.io*;
+import java.util.*;
+public class FileReader
+{
+  //create or overwrite a file, given its name, for storing todoItems
+  public static void storeData(ArrayList<todoItem> list, String filename)
+  {
+    String data = "";
+    Iterator<todoItem> iter = list.iterator();
+    todoItem temp;
+    
+  }
+  
+  //return a list of todoItems given a file providing information
+  public static ArrayList<todoItem> readData(String filename)
+  {
+  }
+}
